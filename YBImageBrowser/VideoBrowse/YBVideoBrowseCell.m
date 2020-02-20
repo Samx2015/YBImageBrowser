0221ae455ce593e8da3ddaa706fa49682c76fc14
Sam <samx2015@hotmail.com>
2020-02-20 01:54:44 -0800

    Updates and fixes

--- YBImageBrowser/VideoBrowse/YBVideoBrowseCell.m
+++ YBImageBrowser/VideoBrowse/YBVideoBrowseCell.m
@@ -510,7 +510,7 @@
         case AVAudioSessionRouteChangeReasonNewDeviceAvailable:
             break;
         case AVAudioSessionRouteChangeReasonOldDeviceUnavailable:
-            if (self->_player && self->_isPlaying) {
+            if (self->_player && self->_playing) {
                 //TODO: merge conflict: took ours
                 //                [self->_player pause];
                 [self.actionBar pause];
